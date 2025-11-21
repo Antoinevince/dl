3b8f193f6555e5400e57de7ed462c86cba247497
Antoinevince <antoinevincent318@icloud.com>
2025-11-21 23:56:46 +0100

    mathematical functions added

--- model/mathfunction.py
+++ model/mathfunction.py
@@ -1,10 +1,30 @@
 import math
 
 def Relu(x):
+    """
+    x: a random real number
+    """
     return 0.5*(x+abs(x))
 
 def logistic(x):
+    """
+    x: a random real number
+    """
     denom = (1+ math.exp(x))**(-1)
     return (math.exp(x))*denom
+
+def gudermannian(x):
+    """
+    x: a random real number
+    """
+    return 2*(math.atan(math.tanh(x/2)))
+
+def algebraic(x, k):
+    """
+    x: a strictly positive real number
+    k: a random real number which is the parameter of the algebraic function taking x as an argument
+    """
+    denom = (1+math.abs(x)**k)**(-(1/k))
+    return x*denom
 
 
