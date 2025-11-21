55912ef87ac1ea198d311cbd74c6c3dbbb39f051
Antoinevince <antoinevincent318@icloud.com>
2025-11-21 23:46:46 +0100

    edits

--- model/mathfunction.py
+++ model/mathfunction.py
@@ -1,11 +1,10 @@
-import torch
-import torch.nn
+import math
 
+def Relu(x):
+    return 0.5*(x+abs(x))
 
-def Relu():
-    pass
-
-def sigmoid():
-    pass
+def logistic(x):
+    denom = (1+ math.exp(x))**(-1)
+    return (math.exp(x))*denom
 
 
