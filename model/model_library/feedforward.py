7ca2207bc4c33f866c077fd5365b7e638e72ae34
Antoinevince <antoinevincent318@icloud.com>
2025-11-22 15:52:21 +0100

    single neuron added

--- model/model_library/feedforward.py
+++ model/model_library/feedforward.py
@@ -1,12 +1,18 @@
 import model.mathfunction as mathfunction
 
 
-class FeedForwardNeuralNetwork():
+class MultiLayerPerceptron():
 
     def __init__(self):
         pass
         
-    def perceptron(self, vector):
-        pass
+    def neuron(self, inputs):
+        """single neuron implemented"""
+
+
+
+        input_sum = 0
+        for k in inputs:
+            input_sum+=k
 
 
