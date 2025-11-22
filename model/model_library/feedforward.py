863c35bc4a39e1b673b7f3923e2548e6f346ca42
Antoinevince <antoinevincent318@icloud.com>
2025-11-22 20:37:02 +0100

    22/11

--- model/model_library/feedforward.py
+++ model/model_library/feedforward.py
@@ -4,12 +4,32 @@ import math
 
 class MultiLayerPerceptron():
 
-    def __init__(self):
-        pass
+    def __init__(self, input_weights, input_biases, hidden_weights, hidden_biases, output_weights, output_biases):
+
+        self.input_weights = input_weights
+        self.input_biases = input_biases
+
+        self.hidden_wheights = hidden_weights
+        self.hidden_biases = hidden_biases
+
+        self.output_weights = output_weights
+        self.output_biases = output_biases
+
+
+
         
-    def input_neuron(self, input_values, activation_function):
+    def input_neuron(self, input_values, id):
         """single neuron implemented"""
+
+        sum = input_values[id]*self.input_weights[id] + self.input_biases[id]
+        new_value = mathfunction.Relu(sum)
+
+        return new_value
+    
+    def train(self):
         pass
+
+      
 
 
         
