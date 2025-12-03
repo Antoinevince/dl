8464773f801ea37b1992f074bb16edd48739d71b
Antoinevince <antoinevincent318@icloud.com>
2025-12-03 19:30:58 +0100

    3/12

--- model/model_library/linearregression.py
+++ model/model_library/linearregression.py
@@ -1,16 +1,20 @@
 import math
 import os
-
+import sys
+sys.path.append("")
 
 from mathfunction import functions
 
 class singleperceptron():
-    def __init__(self, input, weights):
+    def __init__(self, input, weights, bias):
         self.input = input
         self.weights = weights 
+        self.bias = bias
 
     def train(self, loss_function, pas):
         data = self.input
+        ecart = pas
+        
         while ecart>=0:
             
             #initialisation des pentes des différents paramètres
@@ -19,36 +23,37 @@ class singleperceptron():
             
             #calcul des dérivées partielles
             for k in range(len(data[0])):
-                weight_slope += (a*data[0][k] - data[1][k])*2*data[0][k]
-                bias_slope += (a*data[0][k] - data[1][k])*2
+                weight_slope += (self.weights*data[0][k] - data[1][k])*2*data[0][k]
+                bias_slope += (self.weights*data[0][k] - data[1][k])*2
             
             #pondération des pentes
             weight_slope = weight_slope/len(data[0])
             bias_slope = bias_slope/len(data[0])
             
             #ajustement des coefficients
-            a -= weight_slope*pas
-            b -= bias_slope*pas
+            self.weights -= weight_slope*pas
+            self.bias -= bias_slope*pas
         
+            c = loss_function()
             old_c = c
             
             #ajustement du pas
-            #pas = pas/10
+            pas = pas/10
             
             #calcul de la fonction de perte
             for l in range(len(data[1])):
-                c+= (a*data[0][l] + b)**2
+                c+= (self.weights*data[0][l] + self.bias)**2
                 
             c = c/(len(data[0]))
             
             ecart = old_c - c
             
-        return (a, b, c)
+        return (self.weights, self.bias, c)
 
     def neuron(self):
         return 
 
 
 data = [(1, 1.01), (2, 2.004), (3, 3), (4, 5.26)]
-neuron = singleperceptron(data, 0)
+neuron = singleperceptron(data, 1, 1)
 print(neuron.train(functions.quandratic_loss, 0.1))
