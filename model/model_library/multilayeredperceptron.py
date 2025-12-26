d83d576fa15d35763a900f49b92523e988dd198e
Antoinevince <antoinevincent318@icloud.com>
2025-12-26 20:28:57 +0100

    26/12

--- model/model_library/multilayeredperceptron.py
+++ model/model_library/multilayeredperceptron.py
@@ -1,11 +1,47 @@
 import math
+import random
+import numpy
 import sys
+
+import scipy
+
+
+
+
 sys.path.append("")
 from mathfunction import functions
 
+
+class SinglePerceptron():
+    def __init__(self, input, weight, bias, error):
+        self.input = input
+
+        self.weight = weight 
+        self.bias = bias
+
+        self.error= error
+
+    def output(self):
+        return (self.weight*self.input +self.bias)
+    
+    def training(self, learning_rate):
+
+        self.weight = random.randfloat(100)
+        self.bias = random.randfloat(100)
+
+        gradient = numpy.gradient([self.output(k) for k in self.input])
+
+        while gradient >= self.error:
+            diff_weights = learning_rate*self.weights
+            self.weights += diff_weights
+
+            diff_bias = learning_rate*self.bias
+            self.weights += diff_bias
+
+
 class MultiLayerPerceptron():
 
-    def __init__(self, input_weights, input_biases, hidden_weights, hidden_biases, output_weights, output_biases):
+    def __init__(self, layers, input_weights, input_biases, hidden_weights, hidden_biases, output_weights, output_biases):
 
         self.input_weights = input_weights
         self.input_biases = input_biases
@@ -16,6 +52,8 @@ class MultiLayerPerceptron():
         self.output_weights = output_weights
         self.output_biases = output_biases
 
+        self.layers = layers
+
 
 
         
@@ -25,9 +63,18 @@ class MultiLayerPerceptron():
         sum = input_values[id]*self.input_weights[id] + self.input_biases[id]
         new_value = functions.Relu(sum)
 
-        return new_value
+        return sum
     
-    def train(self):
+    def sgd(self, loss_function):
+        initial_weights = [random.randfloat(100)]*self.layers
+        initial_bias = [random.randfloat(100)]*self.layers
+
+        weights = initial_weights
+        bias = initial_bias
+
+        #while loss_function()
+
+        
         pass
 
       
